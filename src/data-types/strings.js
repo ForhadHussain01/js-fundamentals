cc4b40d62997dd5dfb8bf2c489686b42abe73451
Forhad Hussain <furhadhussain@gmail.com>
2022-07-06 15:35:40 +0100

    added answers for strings exercise

--- src/data-types/strings.js
+++ src/data-types/strings.js
@@ -6,13 +6,13 @@ const secondName = 'Smith'
 // TODO: Update the code using Javascript string operations and the variables above so that the tests pass.
 
 // Set this variable to firstName and secondName concatenated
-const fullName = firstName.concat(' ',secondName)
+const fullName = `${firstName} ${secondName}`
 
 // Set this variable to the 10th character of the alphabet variable
-const tenthCharacterOfAlphabet = alphabet.charAt(9)
+const tenthCharacterOfAlphabet = alphabet[9]
 
 // Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = alphabet.toLowerCase('abcdefghijklmnopqrstuvwxyz')
+const lowerCaseAlphabet = alphabet.toLowerCase()
 
 // Set this variable by using a property on the alphabet variable to get it's length
 const numberOfLettersInAlphabet = alphabet.length
